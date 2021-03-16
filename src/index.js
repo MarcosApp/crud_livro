bab149b6303c68e2218e2964e084d8c84f421561
Marcos Gotado <masgotado@apoioprodesp.sp.gov.br>
2021-03-15 21:03:17 -0300

    Tratamento Request

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 const porta = 3000;
 app.set('port', porta);
 
-let contador = 3; // logo após as instruções require
+let contador = 2; // logo após as instruções require
 
 const livros = [
     {
@@ -18,14 +18,16 @@ const livros = [
         titulo: 'Livro 1',
         descricao: 'Descrição Livro 1',
         edicao: 'Edicao Livro 1',
-        autor: 'Autor Livro 1'
+        autor: 'Autor Livro 1',
+        isbn: 123
     },
     {
         id: 2,
         titulo: 'Livro 2',
         descricao: 'Descrição Livro 2',
         edicao: 'Edicao Livro 2',
-        autor: 'Autor Livro 2'
+        autor: 'Autor Livro 2',
+        isbn: 1234
     }
 ]
 
@@ -38,7 +40,11 @@ app.get('/livros', (req, res, next) => {
 // Adiciona Livros
 app.post('/livros', (req, res, next) => {
     const livro = req.body;
-    livros.push({id: contador += 1, titulo: livro.titulo, descricao: livro.descricao, edicao: livro.edicao, autor: livro.autor});
+    let findLivro = livros.find(x => x.isbn == req.body.isbn);
+    if(findLivro)
+        res.status(400).json({message: 'ISBN Existente'});
+
+    livros.push({id: contador += 1, titulo: livro.titulo, descricao: livro.descricao, edicao: livro.edicao, autor: livro.autor, isbn: livro.isbn});
     res.status(201).json(livros);
 })
 
@@ -47,9 +53,9 @@ app.put('/livros/:id', (req, res, next) => {
     const { id } = req.params;
     var index = livros.findIndex(x => x.id == id)
     if(index == -1)
-        res.status(204).json({message: 'Livro não encontrado'});
+        res.status(400).json({message: 'Livro não encontrado'});
 
-    livros[index] = {id: parseInt(id), titulo: req.body.titulo, descricao: req.body.descricao, edicao: req.body.edicao, autor: req.body.autor}
+    livros[index] = {id: parseInt(id), titulo: req.body.titulo, descricao: req.body.descricao, edicao: req.body.edicao, autor: req.body.autor, isbn: req.body.isbn}
     res.status(200).json(livros);
 })
 
@@ -58,7 +64,7 @@ app.delete('/livros/:id', (req, res, next) => {
     const { id } = req.params;
     var index = livros.findIndex(x => x.id == id)
     if(index == -1)
-        res.status(204).json({message: 'Livro não encontrado'});
+        res.status(400).json({message: 'Livro não encontrado'});
 
     livros.splice(index, 1);
     res.status(200).json(livros);
